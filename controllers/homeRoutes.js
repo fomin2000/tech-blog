6d9d109a6089ec9fa2cef6108c782d061103e1b6
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2023-03-04 20:47:00 -0800

    added schema and one get route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+
+router.get('/', (req, res) => {
+
+    res.render('home', {
+        logged_in: req.session.logged_in
+    })
+})
---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logoutBtn = document.getElementById('logoutBtn')
+
+
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+};
+
+logoutBtn.addEventListener('click', logout)
+
